4e0f87d2c375ccc713b95025b9bb0bb431a6965b
hdarjus <hdarjus@gmail.com>
2017-08-20 08:49:20 +0200

    commit changes on cluster, simulations.R was crap here

--- thesis/simulations/simulations.R
+++ thesis/simulations/simulations.R
@@ -10,13 +10,13 @@ iN <- 1000
 dPhi <- 0.95
 dMu <- -9
 
-params <- data.frame(n = NA_integer_, phi = NA_real_, mu = NA_real_, rho = NA_real_, sigma = NA_real_)
+params <- data.frame(n = NA_integer_, phi = NA_real_, mu = NA_real_, rho = NA_real_, sigma2 = NA_real_)
 dat <- list()
 
 for (dRho in c(-0.9, -0.6, -0.3, 0, 0.3, 0.6, 0.9)) {
-  for (dSigma in c(0.01, 0.1)) {
-    params <- rbind(params, c(iN, dPhi, dMu, dRho, dSigma))
-    dat[[length(dat) + 1]] <- fnGenLogSV(iN, dRho, dSigma, dPhi, dMu)
+  for (dSigma2 in c(0.01)) {
+    params <- rbind(params, c(iN, dPhi, dMu, dRho, dSigma2))
+    dat[[length(dat) + 1]] <- fnGenLogSV(iN, dRho, dSigma2, dPhi, dMu)
   }
 }
 
@@ -28,20 +28,17 @@ saveRDS(dat, paste0("gamma_project_dat_", ID, ".RDS"))
 # Runs
 
 iNsim <- 100000
-results <- list()
 
 i <- ID
-ind <- length(results) + 1
-lInit <- list(phi = params[i, "phi"], sigma2 = (params[i, "sigma"])^2, rho = params[i, "rho"], mu = params[i, "mu"])
-results[[ind]] <- list()
+lInit <- list(phi = params[i, "phi"], sigma2 = params[i, "sigma2"], rho = params[i, "rho"], mu = params[i, "mu"])
 lPriors <- list(mu.mean = -9, mu.var = 1, phi.a = 20, phi.b = 1.5,
                 sigma2.shape = 2.25,  # prior mean 0.05 and variance 0.01
                 sigma2.rate = 0.0625,
                 rho.a = 1, rho.b = 1)
-results[[ind]] <- fnMCMCSampler(dat[[i]]$y, iNsim,
+results <- fnMCMCSampler(dat[[i]]$y, iNsim,
                      lInit = lInit, lPriors = lPriors,
                      iBurnin = iNsim %/% 2, sSigma2.Prior = "inv.gamma")
 
-results[[ind]]$h <- t(apply(result[[ind]]$h, 2, function (x, probs) quantile(x, probs = probs), c(.01, .05, .1, .25, .5, .75, .9, .95, .99)))
+results$h <- t(apply(results$h, 2, function (x, probs) quantile(x, probs = probs), c(.01, .05, .1, .25, .5, .75, .9, .95, .99)))
 
 saveRDS(results, paste0("gamma_project_results", ID, ".RDS"))
